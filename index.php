85d9be772038c8091b25febd68afa9802d314bfe
Riki420 <girottiriccardo1998@gmail.com>
2021-09-23 14:14:42 +0200

    init

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+//--------------------------------------------------------------------------------------//
+//                                       *SNACK 1                                       //
+//--------------------------------------------------------------------------------------//
+
+/*Creare una funzione che restituisce un array con 15 numeri casuali,
+tenendo conto che l’array non dovrà contenere lo stesso numero più di una volta.
+Stampare i numeri in pagina.*/
+?>
+
+<?php
+//Creo un array vuoto
+$randomNumbers = [];
+
+
+
+?>
+
