9488c033379b0671f1406180d239c6d0d33d3aa3
Riki420 <girottiriccardo1998@gmail.com>
2021-09-23 15:29:42 +0200

    creato array studenti

---
+++ snack-2/index.php
@@ -0,0 +1,36 @@
+<?php
+//--------------------------------------------------------------------------------------//
+//                                       *SNACK 2                                       //
+//--------------------------------------------------------------------------------------//
+
+/*Creare un array contenente qualche alunno di un’ipotetica classe.
+Ogni alunno avrà Nome, Cognome e un array contenente i suoi voti scolastici (senza, materia, solo numeri). Creare una funzione che calcoli la media voto di un alunno.
+Stampare Nome, Cognome e la media dei voti di ogni alunno.*/
+?>
+
+
+
+<?php
+
+$hypotheticStudents = [
+    [
+        "firstname" => "Papa",
+        "surname" => "Francesco",
+        "vote" => [3, 4, 5, 2, 5, 4, 4]
+    ],
+    [
+        "firstname" => "Silvio",
+        "surname" => "Berlusconi",
+        "vote" => [8, 7, 5, 6, 5, 7, 7]
+    ],
+    [
+        "firstname" => "Maurizio",
+        "surname" => "Costanzo",
+        "vote" => [8, 10, 7, 6, 7, 10, 10]
+    ],
+];
+
+
+
+
+?>
